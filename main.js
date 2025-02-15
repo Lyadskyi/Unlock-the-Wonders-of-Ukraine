25e4228d62a2f812acd3672063aeccf06f4103ca
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2025-02-15 22:24:24 +0200

    Adding access between photos in Swiper

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ $(window).scroll(function () {
   console.log(scrolled);
 
   if (scrolled > 200) {
-    $("#back_to_top").addClass("active");
+    $('#back_to_top').addClass('active');
   } else {
-    $("#back_to_top").removeClass("active");
+    $('#back_to_top').removeClass('active');
   }
 });
 
-$("#back_to_top").click(function () {
-  $("body,html").animate({ scrollTop: 0 }, 10);
+$('#back_to_top').click(function () {
+  $('body,html').animate({ scrollTop: 0 }, 10);
 });
 
-new Swiper(".image-slider");
+const swiperImageSlider = new Swiper('.image-slider', { slidesPerView: 3, spaceBetween: 16 });
+
+const swiperMain = new Swiper('.swiper', { slidesPerView: 3, spaceBetween: 16 });
+
+const arrows = document.querySelectorAll('.arrow');
+
+arrows.forEach(arrow => {
+  arrow.addEventListener('mousedown', () => {
+    arrow.classList.add('active');
+  });
+
+  arrow.addEventListener('mouseup', () => {
+    arrow.classList.remove('active');
+  });
+});
